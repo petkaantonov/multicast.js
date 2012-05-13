0a098829cb7754233a66cd3c00e53b25b8834204
Petka Antonov <petka_antonov@hotmail.com>
2012-05-14 00:22:32 +0300

    opt in for simple function overloading, remove += hacks.

--- multicast.js
+++ multicast.js
@@ -1,72 +1,17 @@
 (function( window, undefined ) {
 
-    var rundefined = /^[\D\d]*?undefined/,
-        rmatchname = /function\s*([a-zA-Z$_][a-zA-Z0-9$_]*)/;
-
-    window.delegate = action;
-    
-    var getFnName = function() {
-    
-        function nameGetter(fn) {
-            return fn.name || "";
-        }
-        
-        if( nameGetter.name && nameGetter.name === "nameGetter" ) {
-            return nameGetter;
-        }
-        
-        return function(fn) {
-            return (((fn+"").match( rmatchname ) || [] )[1] || "");
-        }
-    
-    }();
-    
-    var isArray = function() {
-        var toString = {}.toString;
-        if( Array.isArray ) {
-            return Array.isArray;
-        }
-        else {
-            return function( obj ) {
-                return toString.call( obj, obj ) === "[object Array]";
-            };
-        }
-    }();
-    
-    var defineGetterAndSetter = function() {
-    
-        if( Object.defineProperty ) {
-            return function(obj, name, getter, setter) {
-                Object.defineProperty( obj, name, {
-                    set: setter,
-                    get: getter
-                });
-            };
-        }
-        
-        else if( obj.__defineSetter__ ) {
-            return function(obj, name, getter, setter) {
-                obj.__defineGetter__( name, getter );
-                obj.__defineSetter__( name, setter );
-            };
-        }
-        
-        else {
-            return function(){};
-        }
-            
-    }();
-    
+    window.Observable = observable;
 
     function observe(fn) {
         this.listeners.push(fn);
     }
     
     function unobserve(fn) {
-        var index, name = fn.call ? getFnName(fn) : fn,
+        var index,
             listeners = this.listeners,
             listener,
-            i, len = listeners.length;
+            i,
+            len = listeners.length;
             
         if( !fn ) {
             return null;
@@ -74,7 +19,7 @@
 
         for( i = 0; i < len; ++i ) {
             listener = listeners[i];
-            if( listener === fn || ( name && getFnName( listener ) === name ) ) {
+            if( listener === fn ) {
                 return listeners.splice( i, 1 )[0];
             }
         }
@@ -87,88 +32,38 @@
                 len = listeners.length,
                 i;
     
-    
             for (i = 0; i < len; ++i) {
                 listeners[i].apply(this.context, arguments);
             }
     
     }
     
-    function getter() {
-        return this;
-    }
     
-    function setter( fn ) {
-        if (fn === this) {
-            return;
+    function observable( context ) {
+        if( !context ) {
+            context = this;
         }
-
-        this.observe(new Function("return " + fn.replace(rundefined, ""))());    
-    }
-    
-
-    
-    function toString() {
-        return "";
-    }
-
-    function action( context, name ) {
-        if( isArray( name ) ) {
-            return actions( context, name );
-        }
-        var self = function() {
-            self.update.apply( self, arguments );
+        
+        function self ( fn ) {
+            if( fn && fn.call ) {
+                return observe.call( self, fn);
+            }
+            return update.apply( self, arguments );
         };
 
         self.listeners = [];
         self.context = context;
-
-        self.observe = observe;
-        self.unobserve = unobserve;
-        self.update = update;
-        self.toString = toString;
-
-        defineGetterAndSetter( context, name, getter.bind(self), setter.bind(self) );
-
-        return self;   
+        self.remove = unobserve;
+        return self;
     }
     
-    function actions( context, names ) {
-        names.forEach( function( name ){
-            action( context, name );
-        });
-    }
-
 })( this );
 
+var obj = {};
+obj.clicked = Observable(obj);
 
-function Widget() {
-    delegate(this, "onClose");
-}
+obj.clicked( function(data, hi){
+    console.log( this, arguments );
+} );
 
-Widget.prototype = {
-
-
-    method1: (function() {
-        return function(a) {
-            alert(a);
-        };
-    })(),
-
-    method2: function(a) {
-        alert(a);
-    },
-
-    constructor: Widget
-};
-var a = new Widget();
-
-var a = new Widget();
-
-a.onClose += function() {
-  console.log( this, arguments);  
-};
-
-a.onClose("hai");
-
-a.onClose += function named(){}
+obj.clicked("hi");
